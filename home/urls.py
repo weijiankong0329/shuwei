24699da731c856ecc843e0b8d06fc11175ca0b27
Ma Li <3377320340mali@gmail.com>
2023-08-21 20:48:18 +0800

    首页链接

--- home/urls.py
+++ home/urls.py
@@ -33,7 +33,7 @@ urlpatterns = [
 
     path('经训/', views.JingXun, name='jingxun'),
     path('经训/所有章节/<int:经训_id>/', views.show_all_chapters, name='show_all_chapters'),
-    path('经训/内容/<int:章节_id>/', views.JingXuncontent, name='content'), 
+    path('经训/内容/<int:章节_id>/', views.JingXuncontent, name='jingxundetail'), 
     path('经训/搜索结果/', views.search, name='search'),
     path('书库/', views.ShuKu, name='shuku'),
     path('书库/<int:shuku_id>/', views.ShuKucontent, name='detail'),
